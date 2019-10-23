cb91e2dc664757ba16d167b6491c5e39b3712956
jecaudal <jecaudal@42.fr>
2019-10-23 16:20:08 +0200

    Improvements : 1 : out-put directories creation if it doesn't exists. 2 : Logging of differences and adding of human reading loggig on this. 3 : Makefile and grademe muting about commands executed. Rest to do : Welcome message (Author and friendly message), human readable logging to display steps of the scripts and tests, ending message, tests executed and others tests to do manually.; Portable version of my test after im sure of my sources files.; Script option --help; --clean to delete all binary, output directories, deeptough; Search for other tests files;

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ _SRCS		=	$(addprefix ./src/, $(SRCS))
 
 OBJS		=	$(_SRCS:.c=.o)
 
-CC			=	cc
+CC			=	@cc
 
 CFLAGS		+=	-W -Wall -Wextra -Werror  -D BUFFER_SIZE=$(S)
 
@@ -17,38 +17,15 @@ RM			=	rm -f
 all			:	$(NAME)
 
 $(NAME)		:	$(OBJS)
-				$(CC) -o $(NAME) $(OBJS)
-
-
+				@$(CC) -o $(NAME) $(OBJS)
 
 clean		:
-				$(RM) $(OBJS)
+				@$(RM) $(OBJS)
 
 fclean		:	clean
-				$(RM) $(NAME)
+				@$(RM) $(NAME)
 
 re			:	fclean all
 
-test_s_0	: 	clean $(OBJS)
-				gcc -o ./expected_output/get_next_line_teacher -W -Wall -Wextra -Werror $(OBJS)
-
-test_s_1	:	clean $(OBJS)
-				gcc -o ./expected_output/get_next_line_teacher -W -Wall -Wextra -Werror $(OBJS)
-
-test_s_2	:	clean $(OBJS)
-				gcc -o ./expected_output/get_next_line_teacher -W -Wall -Wextra -Werror $(OBJS)
-
-test_s_10	:	clean $(OBJS)
-				gcc -o ./expected_output/get_next_line_teacher -W -Wall -Wextra -Werror $(OBJS)
-
-test_s_52	:	clean $(OBJS)
-				gcc -o ./expected_output/get_next_line_teacher -W -Wall -Wextra -Werror $(OBJS)
-
-test_s_100	:	clean $(OBJS)
-				gcc -o ./expected_output/get_next_line_teacher -W -Wall -Wextra -Werror $(OBJS)
-
-test_s_1000	:	clean $(OBJS)
-				gcc -o ./expected_output/get_next_line_teacher -W -Wall -Wextra -Werror $(OBJS)
-
-test_s_1000000	: clean $(OBJS)
-				gcc -o ./expected_output/get_next_line_teacher -W -Wall -Wextra -Werror $(OBJS)
+grademe		: 	clean $(OBJS)
+				@gcc -o ./expected_output/get_next_line_teacher -W -Wall -Wextra -Werror $(OBJS)
---
+++ src/get_next_line_bonus.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jecaudal <jecaudal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/19 18:22:13 by jecaudal          #+#    #+#             */
+/*   Updated: 2019/10/21 13:55:47 by jecaudal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/get_next_line.h"
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+	char	*str;
+
+	i = 0;
+	str = (char*)s;
+	while (i < n)
+	{
+		str[i] = '\0';
+		i++;
+	}
+}
+
+char	*ft_strdup_cn(char *str, char c, int n)
+{
+	int		i;
+	char	*new;
+
+	i = 0;
+	if (!(new = (char*)malloc(sizeof(char) * (ft_strlen_cn(str, c, n) + 1))))
+		return (NULL);
+	while (str && str[i] && str[i] != c && (((n > 0) && (i < n)) || (n < 0)))
+	{
+		new[i] = str[i];
+		i++;
+	}
+	if (n != -1)
+		free(str);
+	new[i] = '\0';
+	return (new);
+}
+
+char	*ft_strcut_c(char *str, char c)
+{
+	int		i;
+	int		j;
+	char	*new;
+
+	i = 0;
+	j = 0;
+	if (!(str))
+		return (NULL);
+	if (ft_is_a_cn(str, c, -1) == 0)
+		return (NULL);
+	while (str[i] != c)
+		i++;
+	i++;
+	new = ft_strdup_cn(&str[i], '\0', -1);
+	free(str);
+	return (new);
+}
+
+char	*ft_strjoin_n(char *s1, char *s2, int n)
+{
+	int		i;
+	int		j;
+	char	*new;
+
+	i = 0;
+	j = 0;
+	if (n == -1 && !(new = (char*)malloc(sizeof(char) *
+	((ft_strlen_cn(s1, '\0', -1) + ft_strlen_cn(s2, '\0', -1) + 1)))))
+		return (NULL);
+	else if (!(new = (char*)malloc(sizeof(char) *
+	((ft_strlen_cn(s1, '\0', -1) + ft_strlen_cn(s2, '\0', n) + 1)))))
+		return (NULL);
+	while (s1[i])
+	{
+		new[i] = s1[i];
+		i++;
+	}
+	while ((n == -1 && s2[j]) || j < n)
+		new[i++] = s2[j++];
+	if (n != -1)
+		free(s1);
+	new[i] = '\0';
+	return (new);
+}

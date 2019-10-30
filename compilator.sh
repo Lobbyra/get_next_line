59771c81a4f6e0e7317d3209c9f01e1fbc26e805
jecaudal <jecaudal@42.fr>
2019-10-30 10:40:41 +0100

    compilator improvments and test_files created

--- compilator.sh
+++ compilator.sh
@@ -1,16 +1,20 @@
-if (($1 == "normal"))
+rm -rf log src test tests sortie_correct test.c test.sh gnlkiller 42TESTERS-GNL
+STR="--extra"
+if [[ "$1" == "$STR" ]];
 then
-	gcc -W -Wall -Wextra -Werror -o get_next_line -D BUFFER_SIZE=$2 main.c get_next_line.c get_next_line_utils.c
-fi
-
-if (($1 == "bonus"))
-then
-	gcc -W -Wall -Wextra -Werror -o get_next_line -D BUFFER_SIZE=$2 main.c get_next_line.c get_next_line_utils.c
-fi
-
-if (($1 == "all"))
-then
-	gcc -W -Wall -Wextra -Werror -o get_next_line -D BUFFER_SIZE=$2 main.c get_next_line.c get_next_line_utils.c
-	gcc -W -Wall -Wextra -Werror -o get_next_line_bonus -D BUFFER_SIZE=$2 main.c get_next_line_bonus.c get_next_line_utils_bonus.c
+	git clone https://github.com/Mazoise/42TESTERS-GNL.git
+	cd 42TESTERS-GNL
+	bash all_tests_with_bonus.sh
+	bash runSTDIN+LEAKS.sh
+	cd ..
+	git clone https://github.com/Sherchryst/gnlkiller.git
+	mv gnlkiller/* .
+	sh test.sh > log
+	diff log sortie_correct
+	rm -rf src test tests sortie_correct test.c test.sh gnlkiller 42TESTERS
+else
+	echo "If you want extra tests type ./compilator --extra."
+	gcc -W -Wall -Wextra -Werror -o get_next_line -D BUFFER_SIZE=$1 main.c get_next_line.c get_next_line_utils.c
+	gcc -W -Wall -Wextra -Werror -o get_next_line_bonus -D BUFFER_SIZE=$1 main.c get_next_line_bonus.c get_next_line_utils_bonus.c
 fi
 

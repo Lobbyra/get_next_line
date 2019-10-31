e1c1d50d60412efb4a51e2f266be6db21ffc72d7
jecaudal <jecaudal@42.fr>
2019-10-31 11:35:16 +0100

    do_test improvement

--- test_files/do_tests.sh
+++ test_files/do_tests.sh
@@ -8,3 +8,13 @@ do
 	printf "\n"
 	let i++
 done
+a=1
+while ((a < 10))
+do
+	echo "[Test with $(echo ./test_files/$a*)]"
+	./get_next_line_bonus $(echo ./test_files/$a*)
+	printf "\n"
+	read -p "Press enter to the next test"
+	printf "\n"
+	let a++
+done
